40d46829a4839161b3eebac7e8895b809b56fd7b
12024749 <e12024749@student.tuwien.ac.at>
2023-06-02 15:08:06 +0200

    Portal 3 & 7 funktioniert

--- Assets/portalScript.cs
+++ Assets/portalScript.cs
@@ -6,7 +6,9 @@ public class portalScript : MonoBehaviour
 {
     public GameObject player1;
     public GameObject player2;
-    bool collided = false;
+    bool collided1 = false;
+    bool collided2 = false;
+
     public Vector3 position;
     public Quaternion rotation;
 
@@ -19,23 +21,48 @@ public class portalScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (collided)
+        if (collided1)
         {
             player1.transform.position = position;
             player1.transform.rotation = rotation;
             Debug.Log("POSITION:"+player1.transform.position);
         }
+        if (collided2)
+        {
+            player2.transform.position = position;
+            player2.transform.rotation = rotation;
+            Debug.Log("POSITION:" + player2.transform.position);
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        collided = true;
+        if (collision == player1.GetComponent<Collider>())
+        {
+            collided1 = true;
+
+        }
+
+        if (collision == player2.GetComponent<Collider>())
+        {
+            collided2 = true;
+
+        }
 
     }
 
     private void OnTriggerExit(Collider collision)
     {
-        collided = false;
+        if (collision == player1.GetComponent<Collider>())
+        {
+            collided1 = false;
+
+        }
+        if (collision == player2.GetComponent<Collider>())
+        {
+            collided2 = false;
+
+        }
     }
 
 }
