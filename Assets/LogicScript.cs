5ede3453fabdb4b25ae4425cf0c6581381ef59b5
12024749 <e12024749@student.tuwien.ac.at>
2023-06-06 13:49:04 +0200

    man kann jetzt mit esc pausieren und wieder weiterspielen

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -11,7 +11,37 @@ public class LogicScript : MonoBehaviour
     public GameObject gameOverScreen;
     public GameObject startGameScreen;
     public GameObject Timer;
-   
+    public bool gamePaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0f; // Setze die Zeit auf 0, um das Spiel zu pausieren
+        gamePaused = true;
+        Debug.Log("Spiel pausiert");
+    }
+
+    private void ResumeGame()
+    {
+        Time.timeScale = 1f; // Setze die Zeit auf 1, um das Spiel fortzusetzen
+        gamePaused = false;
+        Debug.Log("Spiel fortgesetzt");
+    }
+
 
     // Start is called before the first frame update
     void Start()
--- Assets/PlayerControllerScript.cs
+++ Assets/PlayerControllerScript.cs
@@ -16,10 +16,12 @@ public class PlayerControllerScript : MonoBehaviour
     public bool reversedArrows = false;
     public bool isJumping = false;
     public bool canJump = true;
+    public LogicScript logic;
 
 
     Rigidbody r;
     float gravity = 10.0f;
+    private Vector3 currentVelocity;
 
     void Awake()
     {
@@ -31,92 +33,92 @@ public class PlayerControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move Front/Back
-        Vector3 targetVelocity = Vector3.zero;
-        if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.W)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.UpArrow)))
-        {
-            if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
+            if (!logic.gamePaused) { 
+            // Move Front/Back
+            Vector3 targetVelocity = Vector3.zero;
+            if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.W)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.UpArrow)))
             {
-                targetVelocity.z = -1;
+                if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
+                {
+                    targetVelocity.z = -1;
+                }
+                else
+                {
+                    targetVelocity.z = 1;
+                }
             }
-            else
+            else if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.S)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.DownArrow)))
             {
-                targetVelocity.z = 1;
+                if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
+                {
+                    targetVelocity.z = 1;
+                }
+                else
+                {
+                    targetVelocity.z = -1;
+                }
             }
-        }
-        else if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.S)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.DownArrow)))
-        {
-            if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
-            {
-                targetVelocity.z = 1;
-            }
-            else
-            {
-                targetVelocity.z = -1;
-            }
-        }
-        targetVelocity = transform.TransformDirection(targetVelocity);
-        targetVelocity *= movementSpeed;
+            targetVelocity = transform.TransformDirection(targetVelocity);
+            targetVelocity *= movementSpeed;
 
-        // Apply a force that attempts to reach our target velocity
-        Vector3 velocity = r.velocity;
-        Vector3 velocityChange = (targetVelocity - velocity);
-        float maxVelocityChange = 10.0f;
-        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-        velocityChange.y = 0;
-        r.AddForce(velocityChange, ForceMode.VelocityChange);
+            // Apply a force that attempts to reach our target velocity
+            Vector3 velocity = r.velocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+            float maxVelocityChange = 10.0f;
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            r.AddForce(velocityChange, ForceMode.VelocityChange);
 
-        // We apply gravity manually for more tuning control
-        r.AddForce(new Vector3(0, -gravity * r.mass, 0));
+            // We apply gravity manually for more tuning control
+            r.AddForce(new Vector3(0, -gravity * r.mass, 0));
 
 
-        // Rotate Left/Right
-        if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.A)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.LeftArrow)))
-        {
-
-            if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
+            // Rotate Left/Right
+            if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.A)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.LeftArrow)))
             {
-                transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
-            }
-            else
-            {
-                transform.Rotate(new Vector3(0, -14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
-            }
 
-        }
-        else if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.D)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.RightArrow)))
-        {
-            if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
-            {
-                transform.Rotate(new Vector3(0, -14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+                if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
+                {
+                    transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, -14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+                }
+
             }
-            else
+            else if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.D)) || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.RightArrow)))
             {
-                transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+                if ((reversedWASD && playerControls == PlayerControls.WASD) || (reversedArrows && playerControls == PlayerControls.Arrows))
+                {
+                    transform.Rotate(new Vector3(0, -14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, 14, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+                }
             }
-        }
 
         
 
-        // jump
-        if (playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.Space) && canJump || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.Return)) && canJump)
-        {
-            //isJumping = true;
-            canJump = false;
-            r.velocity = Vector3.up * jumpStrength;
-        }
-        
+            // jump
+            if (playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.Space) && canJump || (playerControls == PlayerControls.Arrows && Input.GetKey(KeyCode.Return)) && canJump)
+            {
+                //isJumping = true;
+                canJump = false;
+                r.velocity = Vector3.up * jumpStrength;
+            }
 
-        // jump
-        //if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.Space) && canJump)) {
-        ////r.velocity = Vector3.up * jumpStrength;
-        // canJump = false;
-        // }
-        //
+
+            // jump
+            //if ((playerControls == PlayerControls.WASD && Input.GetKey(KeyCode.Space) && canJump)) {
+            ////r.velocity = Vector3.up * jumpStrength;
+            // canJump = false;
+            // }
+            //
+        }
     }
-
-    
 
 
     private void OnTriggerEnter(Collider collision)
